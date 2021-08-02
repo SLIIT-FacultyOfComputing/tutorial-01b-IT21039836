b5cd7058d0ac3ee3e43a9c77f6d0977671400060
it21039836 <88015102+IT21039836@users.noreply.github.com>
2021-08-02 17:08:15 +0000

    I have completed ex.3

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,19 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int num, count = 1, sum = 0;
+
+  printf("Enter a positive number\n");
+  scanf("%d",&num);
+
+  while(count <= num)
+  {
+    sum = sum + count;
+    count++;
+
+  
+  }
+  printf("Sum=%d",sum);
   
   return 0;
 }
